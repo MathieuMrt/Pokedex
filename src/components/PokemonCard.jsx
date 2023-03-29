5eb9331c55a3ab6046a990d3a2fa5483f63b5f04
Mathieu Mrt <martinmathieu49@gmail.com>
2023-03-29 09:33:37 +0200

    Pokédex commit 2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,33 @@
+const card = {
+  width: "500px",
+  padding: "1rem",
+  backgroundColor: pokemon.color,
+  borderRadius: "5px",
+  boxShadow: "0px 0px 5px gray",
+  color: "white",
+  fontSize: "1.6rem",
+  textAlign: "center",
+  textShadow: "0px 0px 5px gray",
+};
+
 const pokemonList = [
   {
     name: "bulbasaur",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    color: "green",
+  },
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    color: "red",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    color: "blue",
   },
   {
     name: "mew",
@@ -10,11 +35,15 @@ const pokemonList = [
 ];
 
 function PokemonCard() {
-  const pokemon = pokemonList[1];
+  const pokemon = pokemonList[0];
 
   return (
-    <figure>
-      {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt=""></img> : <p>???</p>}
+    <figure style={card}>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name}></img>
+      ) : (
+        <p>???</p>
+      )}
       <figcaption>{pokemon.name}</figcaption>
     </figure>
   );

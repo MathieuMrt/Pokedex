e60ecff7f94d7ca342c81005f842b052aa62431d
Mathieu Mrt <martinmathieu49@gmail.com>
2023-03-30 11:59:32 +0200

    State

--- src/App.jsx
+++ src/App.jsx
@@ -7,52 +7,76 @@ import PokemonCard from "./components/PokemonCard.jsx";
 function App() {
   const pokemonList = [
     {
-      name: "Bulbasaur",
+      name: "Bulbizarre",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
       color: "lightgreen",
+      type: "plante / poison",
+      number: 1,
     },
     {
-      name: "Charmander",
+      name: "Salamèche",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
       color: "lightsalmon",
+      type: "feu",
+      number: 4,
     },
     {
-      name: "Squirtle",
+      name: "Carapuce",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
       color: "lightblue",
+      type: "eau",
+      number: 7,
     },
     {
       name: "Pikachu",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
       color: "#F3D77B",
+      type: "électrique",
+      number: 25,
     },
     {
       name: "Mew",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
       color: "#EFC5CE",
+      type: "psy",
+      number: 151,
     },
     {
       name: "MewTwo",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/150.png",
       color: "#B18AB2",
-    },
-    {
-      name: "mew",
+      type: "psy",
+      number: 150,
     },
   ];
-  const [count, setCount] = useState(0);
+
+  let [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const handleClick = () => {
+    pokemonIndex === 0
+      ? setPokemonIndex((pokemonIndex = pokemonList.length - 1))
+      : setPokemonIndex(pokemonIndex - 1);
+  };
+
+  const handleClick2 = () => {
+    pokemonIndex >= pokemonList.length - 1
+      ? setPokemonIndex((pokemonIndex = 0))
+      : setPokemonIndex(pokemonIndex + 1);
+  };
 
   return (
     <div className="App">
       <div>
-        <PokemonCard pokemon={pokemonList[4]} />
+        <PokemonCard pokemon={pokemonList[pokemonIndex]} />
       </div>
+      <button onClick={handleClick}>Précédent</button>
+      <button onClick={handleClick2}>Suivant</button>
     </div>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,44 +1,55 @@
 import PropTypes from "prop-types";
 
-const card = {
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  width: "500px",
-  padding: "1rem",
-  borderRadius: "10px",
-  boxShadow: "0px 0px 15px gray",
-  color: "white",
-  fontSize: "5rem",
-  textAlign: "center",
-  textShadow: "0px 0px 5px gray",
-};
-
-const cardImg = {
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  width: "90%",
-  height: "50%",
-  marginBottom: "3rem",
-  backgroundColor: "white",
-  borderRadius: "10px",
-  boxShadow: "0px 0px 5px gray",
-};
-
 function PokemonCard(props) {
-  card.backgroundColor = props.pokemon.color;
-
-  PokemonCard.propTypes = {
-    pokemon: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      imgSrc: PropTypes.string.isRequired,
-      color: PropTypes.string.isRequired,
-    }).isRequired,
+  const card = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "500px",
+    padding: "1rem",
+    borderRadius: "10px",
+    boxShadow: "0px 0px 30px gray",
+    color: "white",
+    fontSize: "5rem",
+    textAlign: "center",
+    textShadow: "0px 0px 5px gray",
+    border: "15px solid gold",
   };
+
+  const cardImg = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "95%",
+    height: "50%",
+    marginBottom: "1rem",
+    borderRadius: "10px",
+    background:
+      "linear-gradient(217deg, rgba(255,0,0,.8), rgba(255,0,0,0) 70.71%), linear-gradient(127deg, rgba(0,255,0,.8), rgba(0,255,0,0) 70.71%), linear-gradient(336deg, rgba(0,0,255,.8), rgba(0,0,255,0) 70.71%)",
+    border: "5px solid white",
+  };
+
+  const type = {
+    fontSize: "2rem",
+    color: "black",
+    width: "95%",
+    marginBottom: "30px",
+    borderTop: "1px solid black",
+    borderBottom: "1px solid black",
+  };
+
+  const number = {
+    fontSize: "1.5rem",
+    fontWeight: "bold",
+  };
+
+  card.backgroundColor = props.pokemon.color;
 
   return (
     <figure style={card}>
+      <div style={number}>
+        <p>n°{props.pokemon.number} / 151</p>
+      </div>
       <div style={cardImg}>
         {props.pokemon.imgSrc ? (
           <img
@@ -50,9 +61,21 @@ function PokemonCard(props) {
           <p>???</p>
         )}
       </div>
+      <div style={type}>
+        <p>Type : {props.pokemon.type}</p>
+      </div>
       <figcaption>{props.pokemon.name}</figcaption>
     </figure>
   );
 }
+
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default PokemonCard;
